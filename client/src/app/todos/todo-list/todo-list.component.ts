7ff80f62b580c7333ebf405ffde42185e7a748b1
Nicholas Gilbertson <ninjamipsmurf@gmail.com>
2022-02-17 15:15:37 -0600

    Minor Update before Adding Service

--- client/src/app/todos/todo-list/todo-list.component.ts
+++ client/src/app/todos/todo-list/todo-list.component.ts
@@ -8,6 +8,16 @@ import { Todo } from './todo';
 })
 export class TodoListComponent implements OnInit {
 
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoStatus: boolean;
+  public todoOwner: string;
+  public todoBodyText: string;
+  public todoCategory: string;
+  public viewType: 'card' | 'list' = 'card';
+
+
   constructor() { }
 
   ngOnInit(): void {
