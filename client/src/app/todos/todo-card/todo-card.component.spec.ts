ab6516dd0b80b093b4d51c65606d0851229a0c79
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-21 19:11:15 -0600

    Updated some of the testing files

--- client/src/app/todos/todo-card/todo-card.component.spec.ts
+++ client/src/app/todos/todo-card/todo-card.component.spec.ts
@@ -1,25 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TodoCardComponent } from './todo-card.component';
-
 describe('TodoCardComponent', () => {
-  let component: TodoCardComponent;
-  let fixture: ComponentFixture<TodoCardComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoCardComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodoCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  /*
+   * The `TodoCardComponent` has no actually logic in
+   * it, so there's really nothing to test here.
+   */
 });
