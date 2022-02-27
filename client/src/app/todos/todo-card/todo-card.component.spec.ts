8ade204ce919198d4db3f55470712006d4a4572a
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-27 13:10:49 -0600

    Added testing for todo-profile and fixed description of todo-card tests

--- client/src/app/todos/todo-card/todo-card.component.spec.ts
+++ client/src/app/todos/todo-card/todo-card.component.spec.ts
@@ -1,4 +1,4 @@
-describe('TodoCardComponent', () => {
+describe('TodoCardComponent is trivial, so we do not actually test it', () => {
   /*
    * The `TodoCardComponent` has no actually logic in
    * it, so there's really nothing to test here.
--- client/src/app/todos/todo-profile/todo-profile.component.spec.ts
+++ client/src/app/todos/todo-profile/todo-profile.component.spec.ts
@@ -1,17 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteStub } from '../../../testing/activated-route-stub';
+import { MockTodoService } from '../../../testing/todo.service.mock';
+import { Todo } from '../todo-list/todo';
+import { TodoCardComponent } from '../todo-card/todo-card.component';
 import { TodoProfileComponent } from './todo-profile.component';
+import { TodoService } from '../todo.service';
 
 describe('TodoProfileComponent', () => {
   let component: TodoProfileComponent;
   let fixture: ComponentFixture<TodoProfileComponent>;
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoProfileComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MatCardModule
+      ],
+      declarations: [TodoProfileComponent, TodoCardComponent],
+      providers: [
+        { provide: TodoService, useValue: new MockTodoService() },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoProfileComponent);
@@ -19,7 +35,44 @@ describe('TodoProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should navigate to a specific todo', () => {
+    const expectedTodo: Todo = MockTodoService.testTodos[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `TodoProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+    expect(component.todo).toEqual(expectedTodo);
+  });
+
+  it('should navigate to correct todo when the id parameter changes', () => {
+    let expectedTodo: Todo = MockTodoService.testTodos[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `TodoProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+
+    // Changing the paramMap should update the displayed todo profile.
+    expectedTodo = MockTodoService.testTodos[1];
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+  });
+
+  it('should have `null` for the todo for a bad ID', () => {
+    activatedRoute.setParamMap({ id: 'badID' });
+
+    // If the given ID doesn't map to a todo, we expect the service
+    // to return `null`, so we would expect the component's todo
+    // to also be `null`.
+    expect(component.id).toEqual('badID');
+    expect(component.todo).toBeNull();
+  });
 });
